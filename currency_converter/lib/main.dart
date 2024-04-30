db034c3eedc9c822dcaf9d32d70dd527a97793fd
Jmint <jeffreymintah737@gmail.com>
2024-04-30 22:45:09 +0000

    changes

--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -8,7 +8,7 @@ void main(){
 // State meaning: It is a data that determines how your widget should look like
 // Stateles Widget: This is a kind of widget where your app has less state
 
-// Material Design and Cuperstino designs
+// Material Design and Cupertino designs
 
 class MyApp extends StatelessWidget {
     const MyApp({super.key});
