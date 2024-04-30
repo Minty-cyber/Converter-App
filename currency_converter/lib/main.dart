ff096d3995a3acd02d5bfc49ae842c87f2e3b5ae
Jmint <jeffreymintah737@gmail.com>
2024-04-30 19:13:17 +0000

    changes

--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     Widget build(BuildContext context) {
         return Text('Hello World!!!', 
             textDirection: TextDirection.rtl
-            ,)
+            ,);
 
     }
 }
