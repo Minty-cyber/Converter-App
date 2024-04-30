fe6702b1633c7638d91095a966d172dd4898ff87
Jmint <jeffreymintah737@gmail.com>
2024-04-30 23:09:51 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterialPage extends StatelessWidget{
-    const CurrencyConverterMaterialPage({super.key})
+    const CurrencyConverterMaterialPage({super.key});
 
     @override
     Widget build(BuildContext context){
