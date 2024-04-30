742b6254fa3f4fbab2afc586f5345489ede174f5
Jmint <jeffreymintah737@gmail.com>
2024-04-30 19:12:51 +0000

    changes

--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart'; //Importing some package called material.dart
 
 void main(){
-    runApp(Text('Hello World!!!', 
-    textDirection: TextDirection.rtl
-    ,));
+    runApp();
 }
 
 
 // State meaning: It is a data that determines how your widget should look like
 // Stateles Widget: This is a kind of widget where your app has less state
 class MyApp extends StatelessWidget {
-  
+
     @override
     Widget build(BuildContext context) {
-
+        
 
     }
 }
