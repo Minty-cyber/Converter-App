60a987c23c88e612944c2282fccee9ce66b57ced
Jmint <jeffreymintah737@gmail.com>
2024-04-30 23:08:13 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -6,7 +6,9 @@ class CurrencyCOnverterMaterialPAge extends StatelessWidget{
     @override
     Widget build(BuildContext context){
       return Scaffold(
-        
+          body: Center(
+            
+          )
       );
     }
 }
