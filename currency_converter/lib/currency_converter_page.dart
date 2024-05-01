85846702df3ed081897e1f5e7e6d891c85b1ea5c
Jmint <jeffreymintah737@gmail.com>
2024-05-01 13:39:18 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -52,7 +52,8 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                         )
                         ),
                    ),
-                   
+
+                   TextButton(onPressed: onPressed, child: child)
                   
                   ],
     ),
