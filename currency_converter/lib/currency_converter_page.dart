fff354a9c9e1cf730996ca4b6ae749b2a355eaea
Jmint <jeffreymintah737@gmail.com>
2024-05-01 20:31:02 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -56,7 +56,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
                    Padding(
                      padding: const EdgeInsets.all(10.0),
-                     child: TextButton(onPressed: () {
+                     child: EleButton(onPressed: () {
                         if (kDebugMode) {
                             print('Button Clicked'); 
                         }
