cc45a56d3a4b0307fedebbe60906185e3bfca828
Jmint <jeffreymintah737@gmail.com>
2024-05-01 10:47:09 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -21,7 +21,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                     ), 
                   TextField(
                       style: TextStyle(
-                        color: Colors.
+                        color: Colors.black
                       )
                   )
                   ],
