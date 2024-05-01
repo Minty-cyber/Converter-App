ca47cda9bf2f667a19d0e9af0e946a0ef413075c
Jmint <jeffreymintah737@gmail.com>
2024-05-01 10:07:20 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -10,7 +10,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
               color: Color.fromRGBO(255, 0, 0, 0),
               child:  Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.,
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Text('0')
               ],
