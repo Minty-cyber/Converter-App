78df43bf95bb327eabbbf9990f0ddfc7b5529412
Jmint <jeffreymintah737@gmail.com>
2024-05-01 13:05:35 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -42,7 +42,9 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                           focusedBorder: border,
                           enabledBorder: border,
                       ) ,
-                      keyboardType: TextInputType.number
+                      keyboardType: TextInputType.numberWithOptions(
+                        
+                      )
                   
                   ],
     ),
