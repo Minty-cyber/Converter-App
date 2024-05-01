42e289ee2003de9c611311247f0c7766dacb4930
Jmint <jeffreymintah737@gmail.com>
2024-05-01 10:10:24 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget{
-    const CurrencyConverterMaterialPage({super.key});
+class CurrencyConverterMaterialPage extends StatelessWidget {
+  const CurrencyConverterMaterialPage({super.key});
 
-    @override
-    Widget build(BuildContext context){
-      return const Scaffold(
-           body: Center(
-              child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              
-              children: [
-                Text('0')
-              ],
-                      ) 
-           )
-           
-      );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [Text('0')],
+    )));
+  }
 }
