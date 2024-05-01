2b3b25644120ee79a642fd82f0991e95518dcc51
Jmint <jeffreymintah737@gmail.com>
2024-05-01 10:58:54 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -24,7 +24,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                         color: Colors.white
                       ), 
                       decoration: InputDecoration(
-                          labelText: 'PLease enter the amount in GHS'
+                          labelText: 'PLease enter the amount in GHS',
 
 
                       ) ,
