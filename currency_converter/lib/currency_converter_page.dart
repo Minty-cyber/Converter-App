501673cab03fb9dd77bd1a90556d4fe380bf4d6f
Jmint <jeffreymintah737@gmail.com>
2024-05-01 20:26:01 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -63,7 +63,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                           backgroundColor: MaterialStateProperty.all(Colors.black),
                           foregroundColor: MaterialStateProperty.all(Colors.white),
                           // fixedSize:MaterialStateProperty.all(const Size(double.infinity())),
-                          minimumSize: MaterialStateProperty.all(value),
+                          minimumSize: MaterialStateProperty.all(Size(width, height)),
                           shape: MaterialStateProperty.all(const RoundedRectangleBorder(borderRadius: BorderRadius.zero)), 
                           
                       ),
