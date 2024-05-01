a3b87b857d34c5ab0a5feb76731a26b25bc4fb15
Jmint <jeffreymintah737@gmail.com>
2024-05-01 11:51:21 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -38,7 +38,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                                 style: BorderStyle.solid,
                                 strokeAlign: BorderSide.strokeAlignOutside,
                             )
-                            borderRadius: BorderRadius()
+                            borderRadius: BorderRadius.;
                           ),
 
 
