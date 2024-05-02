434442b4ce3098bd9453b247ad77f237b0110ce5
Jmint <jeffreymintah737@gmail.com>
2024-05-02 15:52:16 +0000

    changes

--- currency_converter/lib/currency_converter_cupertino_page.dart
+++ currency_converter/lib/currency_converter_cupertino_page.dart
@@ -1,83 +1,83 @@
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 
-class CurrencyConverterCupertinoPage extends StatefulWidget {
-  const CurrencyConverterCupertinoPage({super.key});
+// class CurrencyConverterCupertinoPage extends StatefulWidget {
+//   const CurrencyConverterCupertinoPage({super.key});
 
-  @override
-  State<CurrencyConverterCupertinoPage> createState() => _CurrencyConverterCupertinoPageState();
-}
+//   @override
+//   State<CurrencyConverterCupertinoPage> createState() => _CurrencyConverterCupertinoPageState();
+// }
 
-class _CurrencyConverterCupertinoPageState extends State<CurrencyConverterCupertinoPage> {
-  double result = 0;
-  final TextEditingController textEditingController = TextEditingController();
+// class _CurrencyConverterCupertinoPageState extends State<CurrencyConverterCupertinoPage> {
+//   double result = 0;
+//   final TextEditingController textEditingController = TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
-    return  CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        backgroundColor: CupertinoColors.systemGrey3,
-        middle: Text('Currency Converter'),
-      ),
-      backgroundColor: CupertinoColors.systemGrey3,
-        child: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center, 
-                children: [
-                      Text(
-                      '\$ ${result != 0 ? result.toStringAsFixed(2) : result.toStringAsFixed(0)}',
-                      style: const TextStyle(
-                          fontSize: 45,
-                          fontWeight: FontWeight.bold,
-                          // fontStyle: FontStyle.italic,
-                          color: Color.fromARGB(255, 255, 255, 255)
-                      ),
-                      ), 
-                    //Padding and container
-                   Padding(
-                     padding: const EdgeInsets.all(10.0),
-                     child: CupertinoTextField(
-                        controller: textEditingController,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          color: CupertinoColors.black,
-                        ), 
-                        decoration: BoxDecoration(
-                          color: CupertinoColors.white,
-                          border: Border.all(),
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        placeholder:  'Please enter the amount in GHS',
-                        prefix: const Icon(CupertinoIcons.money_dollar),
+//   @override
+//   Widget build(BuildContext context) {
+//     return  CupertinoPageScaffold(
+//       navigationBar: const CupertinoNavigationBar(
+//         backgroundColor: CupertinoColors.systemGrey3,
+//         middle: Text('Currency Converter'),
+//       ),
+//       backgroundColor: CupertinoColors.systemGrey3,
+//         child: Center(
+//             child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.center, 
+//                 children: [
+//                       Text(
+//                       '\$ ${result != 0 ? result.toStringAsFixed(2) : result.toStringAsFixed(0)}',
+//                       style: const TextStyle(
+//                           fontSize: 45,
+//                           fontWeight: FontWeight.bold,
+//                           // fontStyle: FontStyle.italic,
+//                           color: Color.fromARGB(255, 255, 255, 255)
+//                       ),
+//                       ), 
+//                     //Padding and container
+//                    Padding(
+//                      padding: const EdgeInsets.all(10.0),
+//                      child: CupertinoTextField(
+//                         controller: textEditingController,
+//                         textAlign: TextAlign.center,
+//                         style: const TextStyle(
+//                           color: CupertinoColors.black,
+//                         ), 
+//                         decoration: BoxDecoration(
+//                           color: CupertinoColors.white,
+//                           border: Border.all(),
+//                           borderRadius: BorderRadius.circular(5),
+//                         ),
+//                         placeholder:  'Please enter the amount in GHS',
+//                         prefix: const Icon(CupertinoIcons.money_dollar),
 
-                        keyboardType: const TextInputType.numberWithOptions(
-                          decimal: true,
-                          signed: true
-                        )
-                        ),
+//                         keyboardType: const TextInputType.numberWithOptions(
+//                           decimal: true,
+//                           signed: true
+//                         )
+//                         ),
 
-                   ),
+//                    ),
 
-                   Padding(
-                     padding: const EdgeInsets.all(10.0),
-                     child: CupertinoButton(
-                      onPressed: () {
-                        setState(() {
-                          result = double.parse(textEditingController.text)*81;
+//                    Padding(
+//                      padding: const EdgeInsets.all(10.0),
+//                      child: CupertinoButton(
+//                       onPressed: () {
+//                         setState(() {
+//                           result = double.parse(textEditingController.text)*81;
                           
-                        });
-                     }, 
-                        color: CupertinoColors.black,
+//                         });
+//                      }, 
+//                         color: CupertinoColors.black,
                         
-                        child: const Text('Convert')
+//                         child: const Text('Convert')
                      
-                     ),
-                   ),
+//                      ),
+//                    ),
 
                   
-                  ],
-    ),
-    ),
-    );
-  }
+//                   ],
+//     ),
+//     ),
+//     );
+//   }
 
-}
+// }
