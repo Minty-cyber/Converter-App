367e7846c0417d0c92828b42ef69e75f9e1adc55
Jmint <jeffreymintah737@gmail.com>
2024-05-02 10:43:41 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CurrencyConverterMaterialPagee extends StatefulWidget {
+class CurrencyConverterMaterialPage extends StatefulWidget {
   State createState() => _CurrencyConverterMaterialPageState();
 }
 
