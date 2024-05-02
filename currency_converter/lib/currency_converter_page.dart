e416fc84f67c3cb6f030a2a788e36043ca7bba4b
Jmint <jeffreymintah737@gmail.com>
2024-05-02 13:38:21 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -38,7 +38,7 @@ class _CurrencyConverterMaterialPageState
                 mainAxisAlignment: MainAxisAlignment.center, 
                 children: [
                       Text(
-                      result., 
+                      result.toStringAsFixed(fractionDigits), 
                       style: const TextStyle(
                           fontSize: 45,
                           fontWeight: FontWeight.bold,
