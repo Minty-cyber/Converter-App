90dd0b24b92996a82fe18de787b2b7c041d48f1e
Jmint <jeffreymintah737@gmail.com>
2024-05-02 09:53:44 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -31,7 +31,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center, 
                 children: [
                      const Text(
-                      '0', style: TextStyle(
+                      result, style: TextStyle(
                           fontSize: 45,
                           fontWeight: FontWeight.bold,
                           // fontStyle: FontStyle.italic,
