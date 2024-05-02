e480fd39f3f7a51f8cb2b089041f76d9e1fc2e9a
Jmint <jeffreymintah737@gmail.com>
2024-05-02 13:51:11 +0000

    changes

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -38,7 +38,7 @@ class _CurrencyConverterMaterialPageState
                 mainAxisAlignment: MainAxisAlignment.center, 
                 children: [
                       Text(
-                      result!= 0 ? result.toStringAsFixed(2) : result.toStringAsFixed(0), 
+                      '\$ ${result != 0 ? result.toStringAsFixed(2) : result.toStringAsFixed(0)}',
                       style: const TextStyle(
                           fontSize: 45,
                           fontWeight: FontWeight.bold,
